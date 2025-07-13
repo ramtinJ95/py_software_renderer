fdebc31bb97d8a8df99b526772e3e401ecde633b
ramtinJ <ramme95@gmail.com>
2025-07-13 13:25:24 +0200

    Refactor complete

--- src/py_software_renderer/display.py
+++ src/py_software_renderer/display.py
@@ -10,10 +10,16 @@ class Display:
         self.color_buffer = np.zeros((self.WIDTH, self.HEIGHT, 4), dtype=np.uint8)
         self.texture = None
 
+    def color(self, r: int, g: int, b: int, a=255) -> np.ndarray:
+        return np.array([r, g, b, a], dtype=np.uint16)
+
     def render(self):
         if self.texture is None:
             self.texture = self.setup_texture()
-        self.set_color_vectorized(255, 0, 0, 255)
+        # self.set_color_vectorized(255, 0, 0, 255)
+        # self.draw_grid(10)
+        color = self.color(0, 255, 0)
+        self.draw_rectangle(150, 150, 100, 100, color)
         pyray.update_texture(
             self.texture, pyray.ffi.cast("void *", self.color_buffer.ctypes.data)
         )
@@ -34,11 +40,25 @@ class Display:
         self.color_buffer[..., 2] = b
         self.color_buffer[..., 3] = a
 
-    def draw_grid(self):
-        pass
+    def draw_rectangle(
+        self, x: int, y: int, height: int, width: int, color: np.ndarray
+    ):
+        # Clip rectangle if it goes outside the screen
+        x_end = min(x + width, self.color_buffer.shape[1])
+        y_end = min(y + height, self.color_buffer.shape[0])
+        x = max(x, 0)
+        y = max(y, 0)
 
-    def draw_rectangle(self):
-        pass
+        # Fill the rectangle area
+        self.color_buffer[y:y_end, x:x_end] = color
+
+    def draw_grid(self, grid_size: int):
+        grid_color = self.color(100, 100, 100)
+        for y in range(0, self.HEIGHT, grid_size):
+            self.color_buffer[:, y, :] = grid_color
+
+        for x in range(0, self.WIDTH, grid_size):
+            self.color_buffer[x, :, :] = grid_color
 
     def update(self):
         pass
